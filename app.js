421130f2c512771fe53029685ca05f9c122b980c
niobium04 <140097366+niobium04@users.noreply.github.com>
2023-08-22 14:12:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+
+VANTA.DOTS({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  size: 4.10
+})
+
+var words = ['Привет!', 'Я Даниил Жуланов', 'Я учусь в университете нефти и газа', 'Увлекаюсь разработкой сайтов', 'А также пишу на Python'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('.word').text(part);
+  },speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
+
+$(document).ready(function() {
+  $(".btn").click(function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $("#projects").offset().top
+    }, 1000);
+  });
+});
+
